bb988b83851a1fd5cbef58de53e9c8754c0c8dc3
sauqi28 <riza.valasev@peruri.co.id>
2022-11-22 08:16:15 +0700

    Menu Sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Iconic Bootstrap 4.5.0 Admin Template">
   <meta name="author" content="WrapTheme, design by: ThemeMakker.com">
-
   <link rel="icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon">
-
   <link rel="stylesheet" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}">
   <link rel="stylesheet" href="{{asset('vendor/jvectormap/jquery-jvectormap-2.0.3.min.css')}}">
@@ -22,16 +20,10 @@
 
 
 </head>
-
-
 <body data-theme="light" class="font-nunito">
-
   <div id="wrapper" class="theme-cyan">
-
     @include('components.navbar')
-
     @yield('contents')
-
   </div>
   <script src="{{asset('bundles/libscripts.bundle.js')}}"></script>
   <script src="{{asset('bundles/vendorscripts.bundle.js')}}"></script>
