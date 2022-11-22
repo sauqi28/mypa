8cccf63fbf38f9bacc037589b3658d3151fcd650
sauqi28 <riza.valasev@peruri.co.id>
2022-11-22 10:13:47 +0700

    Install Breeze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,35 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<head>
-  <title>Manage Apps Peruri</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Iconic Bootstrap 4.5.0 Admin Template">
-  <meta name="author" content="WrapTheme, design by: ThemeMakker.com">
-  <link rel="icon" href="{{asset('images/favicon.ico')}}" type="image/x-icon">
-  <link rel="stylesheet" href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}">
-  <link rel="stylesheet" href="{{asset('vendor/font-awesome/css/font-awesome.min.css')}}">
-  <link rel="stylesheet" href="{{asset('vendor/jvectormap/jquery-jvectormap-2.0.3.min.css')}}">
-  <link rel="stylesheet" href="{{asset('vendor/morrisjs/morris.css')}}" />
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-  <!-- MAIN CSS -->
-  <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
 
-
-</head>
-<body data-theme="light" class="font-nunito">
-  <div id="wrapper" class="theme-cyan">
-    @include('components.navbar')
-    @yield('contents')
-  </div>
-  <script src="{{asset('bundles/libscripts.bundle.js')}}"></script>
-  <script src="{{asset('bundles/vendorscripts.bundle.js')}}"></script>
-  <script src="{{asset('bundles/apexcharts.bundle.js')}}"></script>
-  <script src="{{asset('bundles/jvectormap.bundle.js')}}"></script> <!-- JVectorMap Plugin Js -->
-  <script src="{{asset('bundles/knob.bundle.js')}}"></script> <!-- Jquery Knob-->
-  <!-- page js file -->
-  <script src="{{asset('bundles/mainscripts.bundle.js')}}"></script>
-  <script src="{{asset('js/pages/blog.js')}}"></script>
-</body>
-
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
 </html>
